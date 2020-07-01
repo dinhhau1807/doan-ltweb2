518874839846ff8e3de05da2be5f931a9169659d
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-07-01 18:59:08 +0700

    login, register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Switch, Router } from 'react-router-dom';
 import { history } from './utils/history';
-import { PrivateRoute } from './layouts/PrivateRoute';
+import { PrivateRoute, PublicRoute } from './layouts';
 import { routes } from './routes';
 
 const renderRoutes = (routes = []) => {
   let result = null;
   if (routes.length > 0) {
-    result = routes.map(({ path, exact, component, layout }, index) => (
-      <PrivateRoute
-        key={index}
-        path={path}
-        exact={exact}
-        component={component}
-        layout={layout}
-      />
-    ));
+    result = routes.map(
+      ({ isPrivate, path, exact, component, layout }, index) =>
+        isPrivate ? (
+          <PrivateRoute
+            key={index}
+            path={path}
+            exact={exact}
+            component={component}
+            layout={layout}
+          />
+        ) : (
+          <PublicRoute
+            key={index}
+            path={path}
+            exact={exact}
+            component={component}
+            layout={layout}
+          />
+        )
+    );
   }
   return <Switch>{result}</Switch>;
 };
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,26 +1,42 @@
 import { lazy } from 'react';
-import { PrivateLayout } from './layouts/PrivateLayout';
+import { PrivateLayout, PublicLayout } from './layouts';
 
 const UserPage = lazy(() => import('./pages/UserPage'));
 const TodoPage = lazy(() => import('./pages/TodoPage'));
 const NotFound = lazy(() => import('./pages/NotFoundPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage'));
 
 export const routes = [
   {
     path: '/',
     exact: true,
+    isPrivate: true,
     layout: PrivateLayout,
     component: UserPage
   },
   {
+    path: '/login',
+    exact: true,
+    layout: PublicLayout,
+    component: LoginPage
+  },
+  {
+    path: '/register',
+    exact: true,
+    layout: PublicLayout,
+    component: RegisterPage
+  },
+  {
     path: '/todo',
     exact: true,
+    isPrivate: true,
     layout: PrivateLayout,
     component: TodoPage
   },
   {
     path: '*',
-    layout: PrivateLayout,
+    layout: PublicLayout,
     component: NotFound
   }
 ];
