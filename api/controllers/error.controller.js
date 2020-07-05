e2668525c98b088763ad7e8735ca2fcef2606994
NDHau <nguyendinhhau1807@gmail.com>
2020-07-05 18:35:01 +0700

    Fix environment error

--- api/controllers/error.controller.js
+++ api/controllers/error.controller.js
@@ -51,9 +51,9 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'developments') {
+  if (process.env.NODE_ENV === 'development') {
     resErrorDevelopment(err, res);
-  } else if (process.env.NODE_ENV === 'development') {
+  } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.message = err.message;
 
