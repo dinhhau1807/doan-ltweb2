73bfae871c7620de8a6d4020931a816e931e2171
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-07-06 11:54:16 +0700

    create staff menu

--- client/src/constants/GlobalConstants.js
+++ client/src/constants/GlobalConstants.js
@@ -1,2 +1,9 @@
 export const TOKEN_KEY = 'token';
 export const LOGIN_URL = '/login';
+
+export const STAFF_TABS = {
+  CUSTOMER: '/',
+  IDENTITY: '/identity',
+  TRANSACTION: '/transaction',
+  STAFF: '/staff'
+};
--- client/src/layouts/PrivateLayout.js
+++ client/src/layouts/PrivateLayout.js
@@ -2,15 +2,19 @@ import React, { Suspense } from 'react';
 import { Spin, Layout } from 'antd';
 import CustomerSidebar from '../components/CustomerSidebar/CustomerSidebar';
 import StaffSidebar from '../components/StaffSidebar/StaffSidebar';
+import Header from '../components/Header/Header';
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 export const PrivateLayout = ({ isStaffRoute, children }) => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       {isStaffRoute ? <StaffSidebar /> : <CustomerSidebar />}
       <Layout>
-        <Header style={{ padding: 0, background: '#fff' }} />
+        <Header
+          style={{ padding: 0, background: '#fff' }}
+          isStaffRoute={isStaffRoute}
+        />
         <Content style={{ margin: '16px' }}>
           <Suspense fallback={<Spin className="spinning"></Spin>}>
             <div className="main">{children}</div>
