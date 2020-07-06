3b04802830fd74d498152f69c861bcc4e35fe0aa
Anh Nguyen Tran <nttanh6299@gmail.com>
2020-07-06 14:36:44 +0700

    create customer menu

--- client/src/constants/GlobalConstants.js
+++ client/src/constants/GlobalConstants.js
@@ -7,3 +7,15 @@ export const STAFF_TABS = {
   TRANSACTION: '/transaction',
   STAFF: '/staff'
 };
+
+export const CUSTOMER_TABS = {
+  ACCOUNT: '/',
+  ACCOUNT_HISTORY: '/history',
+  TRANSACTION: '/transaction',
+  TRANSACTION_EXTERNAL: '/ext-transaction',
+  SAVINGS_REQUEST: '/savings-request',
+  SAVINGS_WITHDRAW: '/savings-withdraw',
+  SAVINGS_TRANSACTION_HISTORY: '/savings-history',
+  UTILS_UPDATE: '/update-infomation',
+  UTILS_PASSWORD: '/change-password'
+};
