9ac70cea1b8aa1c5c2e404d9165a2e3523f845e6
NDHau <nguyendinhhau1807@gmail.com>
2020-07-07 15:00:13 +0700

    Fix: remove alias models

--- api/models/staff.js
+++ api/models/staff.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Staff.belongsTo(models.Role, {
-        as: 'Current',
         foreignKey: 'roleId',
       });
     }
