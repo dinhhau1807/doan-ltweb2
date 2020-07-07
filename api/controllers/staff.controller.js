44da93165a6257eae544076d81a74f762712f1e5
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-07-07 14:56:23 +0700

    Add sort, filter, pagination list customers for staff

--- api/controllers/staff.controller.js
+++ api/controllers/staff.controller.js
@@ -4,6 +4,21 @@ const AppError = require('../utils/appError');
 const { STATUS } = require('../utils/statusEnum');
 const { Customer } = require('../models');
 
+const getPagination = (page, size) => {
+  const limit = size ? +size : 5;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+const getPagingData = (data, page, limit) => {
+  const { count: totalItems, rows: customers } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItems / limit);
+
+  return { totalItems, customers, totalPages, currentPage };
+};
+
 exports.updateStatus = asyncHandler(async (req, res, next) => {
   const { idCustomer, status } = req.body;
   const customer = await Customer.findOne({
@@ -22,4 +37,36 @@ exports.updateStatus = asyncHandler(async (req, res, next) => {
     status: 'success',
     data: { status: 'success' },
   });
+});
+
+exports.getAllCustomer = asyncHandler(async (req, res, next) => {
+  const { page, size, search, sortBy, sortType } = req.query;
+  const attributes = ['username', 'email', 'name', 'phoneNumber', 'address'];
+  const typeSort = ['asc', 'desc'];
+  const condition = search ? { [sortBy]: { [Op.like]: `%${search}%` } } : null;
+  const { limit, offset } = getPagination(page, size);
+
+  if (!attributes.includes(sortBy) || !typeSort.includes(sortType)) {
+    return next(new AppError('Property invalid!', 400));
+  }
+  await Customer.findAndCountAll({
+    attributes: attributes,
+    where: condition,
+    limit,
+    offset,
+    order: [[sortBy, sortType]],
+  })
+    .then((data) => {
+      const listCustomer = getPagingData(data, page, limit);
+      return res.status(200).json({
+        status: 'success',
+        data: { status: 'success', listData: listCustomer },
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        message:
+          err.message || 'Some error occurred while retrieving customers.',
+      });
+    });
 });
