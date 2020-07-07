370eb3cfbb96756ab31576b0fe20cb6bc74f02d4
NDHau <nguyendinhhau1807@gmail.com>
2020-07-07 15:44:54 +0700

    Add unique username staff model

--- api/models/staff.js
+++ api/models/staff.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       username: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
       },
       password: {
         type: DataTypes.STRING,
