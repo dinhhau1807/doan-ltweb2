2fecfe0ec33790d2a89a1e23f7a0824eff22c888
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-07-08 17:18:28 +0700

    Update get all customers and change status for staff

--- api/controllers/staff.controller.js
+++ api/controllers/staff.controller.js
@@ -4,147 +4,80 @@ const AppError = require('../utils/appError');
 const { STATUS } = require('../utils/statusEnum');
 const { Customer } = require('../models');
 
-const getPagination = (page, size) => {
-  const limit = size ? +size : 5;
-  const offset = page ? page * limit : 0;
-
-  return { limit, offset };
-};
-
-exports.updateStatus = asyncHandler(async (req, res, next) => {
-  const { idCustomer, status } = req.body;
+const findCustomer = asyncHandler(async (id) => {
   const customer = await Customer.findOne({
-    where: { [Op.or]: [{ id: idCustomer }] },
-  });
-
-  const newStatus = STATUS[status];
-
-  if (!newStatus || (newStatus && customer.status === newStatus)) {
-    return next(new AppError('Fail update status!', 400));
-  }
-
-  customer.status = newStatus;
-  customer.save();
-  return res.status(200).json({
-    status: 'success',
-  });
-});
-
-exports.getAllCustomer = asyncHandler(async (req, res, next) => {
-  const {
-    page,
-    size,
-    sortBy,
-    sortType = 'desc',
-    username,
-    email,
-    name,
-    phoneNumber,
-    address,
-  } = req.query;
-  const attributes = ['username', 'email', 'name', 'phoneNumber', 'address'];
-  const typeSort = ['asc', 'desc'];
-  const { limit, offset } = getPagination(page, size);
-
-  if (Object.keys(req.query).length === 0) {
-    const customer = await Customer.findAndCountAll({
-      attributes: attributes,
-      limit,
-      offset,
-    });
-    return res.status(200).json({
-      status: 'success',
-      totalItems: customer.count,
-      items: customer.rows,
-    });
-  }
-
-  if (!attributes.includes(sortBy) || !typeSort.includes(sortType)) {
-    return next(new AppError('Property invalid!', 400));
-  }
-
-  if (Object.keys(req.query).length === 1) {
-    const customer = await Customer.findAndCountAll({
-      attributes: attributes,
-      limit,
-      offset,
-      order: [[sortBy, 'desc']],
-    });
-    return res.status(200).json({
-      status: 'success',
-      totalItems: customer.count,
-      items: customer.rows,
-    });
-  }
-
-  if (Object.keys(req.query).length === 2) {
-    const customer = await Customer.findAndCountAll({
-      attributes: attributes,
-      limit,
-      offset,
-      order: [[sortBy, sortType]],
-    });
-    return res.status(200).json({
-      status: 'success',
-      totalItems: customer.count,
-      items: customer.rows,
-    });
-  }
-
-  const customer = await Customer.findAndCountAll({
-    attributes: attributes,
+    attributes: ['username', 'email', 'name', 'phoneNumber', 'address'],
     where: {
-      [Op.or]: [
+      [Op.and]: [
+        { id },
         {
-          username: {
-            [Op.or]: {
-              [Op.like]: `%${username}%`,
-              [Op.eq]: null,
-            },
-          },
-        },
-        {
-          email: {
-            [Op.or]: {
-              [Op.like]: `%${email}%`,
-              [Op.eq]: null,
-            },
-          },
-        },
-        {
-          name: {
-            [Op.or]: {
-              [Op.like]: `%${name}%`,
-              [Op.eq]: null,
-            },
-          },
-        },
-        {
-          phoneNumber: {
-            [Op.or]: {
-              [Op.like]: `%${phoneNumber}%`,
-              [Op.eq]: null,
-            },
-          },
-        },
-        {
-          address: {
-            [Op.or]: {
-              [Op.like]: `%${address}%`,
-              [Op.eq]: null,
-            },
+          status: {
+            [Op.ne]: STATUS.deleted,
           },
         },
       ],
     },
-    limit,
-    offset,
-    order: [[sortBy, sortType]],
   });
+  return customer;
+});
 
+exports.updateCustomerStatus = asyncHandler(async (req, res, next) => {
+  const { idCustomer, status } = req.body;
+  const newStatus = STATUS[status];
+  if (!newStatus || (newStatus && newStatus === STATUS.deleted)) {
+    return next(new AppError('Invalid status!', 400));
+  }
+  const customer = await findCustomer(idCustomer);
+  if (!customer) {
+    return next(new AppError("Can't find this customer!", 404));
+  }
+  customer.status = newStatus;
+  customer.save();
   return res.status(200).json({
     status: 'success',
-    totalItems: customer.count,
-    items: customer.rows,
+    data: customer,
+  });
+});
+
+exports.getAllCustomers = asyncHandler(async (req, res, next) => {
+  let { page, limit, sortBy, sortType } = req.query;
+  const attributes = ['username', 'email', 'name', 'phoneNumber', 'address'];
+  const sortTypes = ['asc', 'desc'];
+
+  if (!page || page <= 0) page = 1;
+  if (!limit || limit <= 0) limit = 10;
+  if (!sortType || (sortType && !sortTypes.includes(sortType)))
+    sortType = 'asc';
+
+  if (!sortBy || (sortBy && !attributes.includes(sortBy))) sortBy = 'username';
+
+  const filterArr = [];
+  attributes.forEach((attr) => {
+    if (req.query[attr]) {
+      const obj = {};
+      obj[attr] = { [Op.like]: `%${req.query[attr]}%` };
+      filterArr.push(obj);
+    }
+  });
+  const customers = await Customer.findAll({
+    attributes: attributes,
+    where: {
+      [Op.and]: [
+        {
+          status: {
+            [Op.ne]: STATUS.deleted,
+          },
+        },
+        ...filterArr,
+      ],
+    },
+    order: [[sortBy, sortType]],
+    offset: limit * (page - 1),
+    limit,
+  });
+  return res.status(200).json({
+    status: 'success',
+    totalItems: customers.length,
+    items: customers,
   });
 });
