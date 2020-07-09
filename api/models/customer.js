1b1b00210500e4b5e01a9fce3c09ad48f677c6ae
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-07-10 00:13:23 +0700

    Add validation register for customer

--- api/models/customer.js
+++ api/models/customer.js
@@ -16,36 +16,82 @@ module.exports = (sequelize, DataTypes) => {
       username: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+				unique: true,
+				validate: {
+					notEmpty: true,
+					isAlphanumeric: true, //Only number + character
+					notIn: {
+						args: [['admin']],
+						msg: "Cant use username: admin"
+					},
+					len: {
+						args: [6, 12],
+						msg: "Username must be between 6 and 12 characters in length"
+					},
+				},
       },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+				unique: true,
+				validate: {
+					notEmpty: true,
+					len: {
+						args: [6, 128],
+						msg: "Email address must be between 6 and 128 characters in length"
+					},
+					isEmail: {
+							msg: "Email address must be valid"
+					} 
+				},
       },
       password: {
         type: DataTypes.STRING,
-        allowNull: false,
+				allowNull: false,
+				validate: {
+					notEmpty: true,
+				},
       },
       name: {
         type: DataTypes.STRING,
-        allowNull: false,
+				allowNull: false,
+				validate: {
+					notEmpty: true,
+					len: {
+						args: [3, 50],
+						msg: "Name must be between 3 and 50 characters in length"
+					},
+				},
       },
       dateOfBirth: {
         type: DataTypes.DATE,
-        allowNull: false,
+				allowNull: false,
+				validate: {
+					notEmpty: true,
+				},
       },
       phoneNumber: {
         type: DataTypes.STRING,
-        allowNull: false,
+				allowNull: false,
+				validate: {
+					notEmpty: true,
+					isNumeric: true,
+					is: {
+						args: /(03|05|07|08|09|[2|6|8|9])+([0-9]{8})\b/,
+						msg: "Phone number invalid"
+					},
+				},
       },
       address: {
         type: DataTypes.STRING,
-        allowNull: false,
+				allowNull: false,
+				validate: {
+					notEmpty: true,
+				},
       },
       verifyCode: {
         type: DataTypes.STRING,
-        allowNull: true,
+				allowNull: true,
       },
       status: {
         type: DataTypes.STRING,
