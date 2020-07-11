739ce1ed3f1162ef15f1bfbfb0a775c6b781300c
Linh Nguyen Vu <nguyenvulinh99@gmail.com>
2020-07-12 00:16:42 +0700

    Add getInfo, updateInfo, ChangePwd for customer

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -1,11 +1,88 @@
 const asyncHandler = require('express-async-handler');
+const jwt = require('jsonwebtoken');
 
-const { Op } = require('sequelize');
-const { Customer, Identity } = require('../models');
+const { Customer } = require('../models');
 const AppError = require('../utils/appError');
+const passwordValidator = require('../utils/passwordValidator');
+
+const signToken = (type, id) => {
+  return jwt.sign({ type, id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 // test method
 exports.getAllCustomers = asyncHandler(async (req, res, next) => {
   const customers = await Customer.findAll();
   res.status(200).json({ status: 'success', data: customers });
+});
+
+exports.getInfo = asyncHandler(async (req, res, next) => {
+  const customer = req.user;
+  const customerInfo = await Customer.findOne({
+    attributes: {
+      exclude: ['password', 'verifyCode'],
+    },
+    where: { id: customer.id },
+  });
+  res.status(200).json({ status: 'success', data: customerInfo });
+});
+
+exports.updateInfo = asyncHandler(async (req, res, next) => {
+  const customer = req.user;
+  const { name, dateOfBirth, phoneNumber, address } = req.body;
+  await Customer.update(
+    {
+      name,
+      dateOfBirth,
+      phoneNumber,
+      address,
+    },
+    {
+      where: { id: customer.id },
+    }
+  );
+  res.status(200).json({
+    status: 'success',
+    message: 'Update information successful.',
+  });
+});
+
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+  const customer = req.user;
+  const { oldPassword, newPassword } = req.body;
+  const matchedOldPwd = await passwordValidator.verifyHashedPassword(
+    oldPassword,
+    customer.password
+  );
+  const matchedNewPwd = await passwordValidator.verifyHashedPassword(
+    newPassword,
+    customer.password
+  );
+  if (!matchedOldPwd) {
+    return next(new AppError('Old password incorrect.', 401));
+  }
+  if (matchedNewPwd) {
+    return next(
+      new AppError('New password must be different from current password.', 401)
+    );
+  }
+
+  const newPasswordHashed = await passwordValidator.createHashedPassword(
+    newPassword
+  );
+
+  await Customer.update(
+    {
+      password: newPasswordHashed,
+    },
+    {
+      where: { id: customer.id },
+    }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Change password successful.',
+  });
 });
