e50ecf2623e54b83565eaa12338c6082a65db0ef
NDHau <nguyendinhhau1807@gmail.com>
2020-08-11 16:18:24 +0700

    Add fake-bank

---
+++ fake-bank/migrations/20200809081507-create-transfer.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transfers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.DECIMAL
+      },
+      currency: {
+        type: Sequelize.STRING
+      },
+      sourceBankId: {
+        type: Sequelize.STRING
+      },
+      destinationBankId: {
+        type: Sequelize.STRING
+      },
+      destinationAccount: {
+        type: Sequelize.STRING
+      },
+      note: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transfers');
+  }
+};
---
+++ fake-bank/models/bank.js
@@ -0,0 +1,37 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Bank extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Bank.init(
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      key: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Bank',
+    }
+  );
+  return Bank;
+};
